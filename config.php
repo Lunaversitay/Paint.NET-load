ba18395a71d7c70533bc73fded1ae3765b2e040a
Luna Versity <mrgodzbrother@gmail.com>
2016-05-09 01:54:12 -0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,29 @@
+<?
+
+/*
+Hey gay retard, you'll need to use ?steamid=%s cause shit works like that, ok? thanks, cya later faggot.
+one more thing, thanks for not wasting $3.00 with a loading screen that doesn't even fit the screen :lenny_face:
+Luna made this cause Lunah is sexy
+Atomik is gay
+beastguy is a faggot i think
+
+and yes... it all changes in-game if you use the right properties (?steamid=%s) so stop fucking asking
+*/
+
+$APIKey = 'google steamapi key'; // Your SteamAPI Key (It's needed to work fam)
+
+// Rules
+// tell people how to live their lifes on your server cause you're more powerful than them
+$rule_1 = 'Rule - Edit in config.php';
+$rule_2 = 'Rule - Edit in config.php';
+$rule_3 = 'Rule - Edit in config.php';
+$rule_4 = 'Rule - Edit in config.php';
+$rule_5 = 'Rule - Edit in config.php';
+$rule_6 = 'Rule - Edit in config.php';
+$rule_7 = 'Rule - Edit in config.php';
+$rule_8 = 'Rule - Edit in config.php';
+$rule_9 = 'Rule - Edit in config.php';
+
+$snazzy_welcome = 'Hey GayRetard'; // the comma is already there by default
+
+?>
